cddca63f074b15b1b63ef2b04c98f0f0dd9db2b0
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-18 01:01:07 +0600

    Delete cart item and sync data using refetch

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,14 +1,16 @@
 import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-orange-400">
                 <ul className="menu p-4">
                     <li><NavLink to={'/dashboard/userHome'}><FaHome></FaHome>user Home</NavLink></li>
                     <li><NavLink to={'/dashboard/reservation'}><FaCalendar></FaCalendar> Reservation</NavLink></li>
-                    <li><NavLink to={'/dashboard/cart'}><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
+                    <li><NavLink to={'/dashboard/cart'}><FaShoppingCart></FaShoppingCart>My Cart ({cart.length})</NavLink></li>
                     <li><NavLink to={'/dashboard/review'}><FaAd></FaAd>Add A Review</NavLink></li>
                     <li><NavLink to={'/dashboard/bookings'}><FaList></FaList> My Bookings</NavLink></li>
                     <div className="divider"></div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,12 +1,41 @@
+import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart] = useCart();
+  const [cart, refacth] = useCart();
+  const axiosSecure = useAxiosSecure();
 
   const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
+  const handleDelete = id => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`)
+        .then(res=>{
+            if(res.data.deletedCount> 0){
+                refacth();
+                   Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+            }
+        })
+        }
+      });
+  }
   return (
     <div>
-      <div className="flex justify-evenly items-center">
+      <div className="flex justify-evenly items-center mb-10">
         <h1 className="text-4xl text-gray-400">Items: {cart.length}</h1>
         <h1 className="text-4xl text-gray-700">
           Total Price: ${totalPrice.toFixed(2)}
@@ -15,60 +44,48 @@ const Cart = () => {
       </div>
 
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
               <th>
-                #
-                {/* <label>
-                  <input type="checkbox" className="checkbox" />
-                </label> */}
+                SI.NO
               </th>
               <th>Image</th>
-              <th></th>
               <th>Name</th>
               <th>Price</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {
-                cart.map(item =>  <tr key={item._id}>
-                    <th>
-                      <label>
-                        <input type="checkbox" className="checkbox" />
-                      </label>
-                    </th>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle h-12 w-12">
-                            <img
-                              src={item.image}
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
-                        <div>
-                          <div className="font-bold">Hart Hagerty</div>
-                          <div className="text-sm opacity-50">United States</div>
-                        </div>
+            {cart.map((item,index) => (
+              <tr key={item._id}>
+                <th>
+                    {index +1}
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
                       </div>
-                    </td>
-                    <td>
-                      Zemlak, Daniel and Leannon
-                      <br />
-                      <span className="badge badge-ghost badge-sm">
-                        Desktop Support Technician
-                      </span>
-                    </td>
-                    <td>Purple</td>
-                    <th>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </th>
-                  </tr>)
-            }
+                    </div>
+                  </div>
+                </td>
+                <td>
+                 {
+                    item.name
+                 }
+                </td>
+                <td>${item.price}</td>
+                <th>
+                  <button onClick={()=>handleDelete(item._id)} className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-600"></FaTrashAlt></button>
+                </th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
