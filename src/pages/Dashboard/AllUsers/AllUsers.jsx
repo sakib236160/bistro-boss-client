efe19c4f53d5bafc84d4a33e5688091beff3601f
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-19 10:31:24 +0600

    Display All users and Create Make admin API

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,15 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const {data: users=[]} = useQuery({
+    const {data: users=[], refacth} = useQuery({
         queryKey: ['users'],
         queryFn: async ()=>{
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleMakeAdmin = user =>{
+      
+    }
+
+    const handleDeleteUser = (user) =>{
+          Swal.fire({
+                  title: "Are you sure?",
+                  text: "You won't be able to revert this!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                  axiosSecure.delete(`/users/${user._id}`)
+                  .then(res=>{
+                      if(res.data.deletedCount> 0){
+                          refacth();
+                             Swal.fire({
+                      title: "Deleted!",
+                      text: "Your file has been deleted.",
+                      icon: "success"
+                    });
+                      }
+                  })
+                  }
+                });
+    }
+
     return (
         <div>
             <div className="flex justify-evenly my-4">
@@ -17,23 +50,29 @@ const AllUsers = () => {
                 <h2 className="5xl">Total Users:{users.length}</h2>
             </div>
             <div className="overflow-x-auto">
-  <table className="table table-zebra">
+  <table className="table table-zebra w-full">
     {/* head */}
     <thead>
       <tr>
         <th></th>
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
       {
-        users.map(user=> <tr key={user._id}>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+        users.map((user,index)=> <tr key={user._id}>
+            <th>{index + 1}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>
+            <button onClick={()=>handleMakeAdmin(user)} className="btn bg-orange-400 btn-lg"><FaUsers className="text-white text-2xl"></FaUsers></button>
+            </td>
+            <td>
+               <button onClick={()=>handleDeleteUser(user)} className="btn bg-red-600 btn-lg"><FaTrashAlt className="text-white"></FaTrashAlt></button>
+            </td>
           </tr>)
       }
     </tbody>
