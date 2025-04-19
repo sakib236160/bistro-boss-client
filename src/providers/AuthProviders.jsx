74739e907735dfa5e9bc4d524deaa43e896cbfb8
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-19 15:14:04 +0600

    Create a JWT token and save it on local storage

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword,signInWithPopup,signOut, updateProfile,GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 // import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 export const AuthContext = createContext(null);
@@ -10,6 +11,7 @@ const AuthProviders = ({children}) => {
     const [user,setUser]=useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password)=>{
         setLoading(true);
@@ -40,7 +42,18 @@ const AuthProviders = ({children}) => {
     useEffect(()=>{
         const unsubscribe= onAuthStateChanged(auth, currentUser=>{
             setUser(currentUser);
-            console.log('Current User', currentUser);
+            if(currentUser){
+                const userInfo = {email: currentUser.email};
+                axiosPublic.post('/jwt', userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+            }else{
+                // tODO: Removed token
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         })
         return()=>{
