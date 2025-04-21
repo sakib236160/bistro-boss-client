0ee2738d231b69de3697cc7949f1b0b1e324dad8
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-21 13:40:08 +0600

    Delete menu item with VerifyAdmin middleware

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,11 +1,40 @@
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import UseMenu from "../../../hooks/UseMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = UseMenu();
+  const [menu, ,refetch] = UseMenu();
+  const axiosSecure = useAxiosSecure();
 
-  const handleDeleteItem = (item) => {};
+  const handleDeleteItem =(item) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+            const res = await axiosSecure.delete(`/menu/${item._id}`);
+            // console.log(res.data);
+            if(res.data.deletedCount > 0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${item.name} has been deleted`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+       
+        }
+      });
+  };
 
   return (
     <div>
