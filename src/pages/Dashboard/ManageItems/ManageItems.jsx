08bc1bf9c28e1afd7371b45ab72adf865cd070b6
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-21 12:11:01 +0600

    Create Manage Items admin route and Load all menu items

---
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,73 @@
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import UseMenu from "../../../hooks/UseMenu";
+
+const ManageItems = () => {
+  const [menu] = UseMenu();
+
+  const handleDeleteItem = (item) => {};
+
+  return (
+    <div>
+      <SectionTitle
+        heading="Manage All Items"
+        subHeading="Hurry Up"
+      ></SectionTitle>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={item._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src={item.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>$ {item.price}</td>
+                  <td>
+                    <button
+                      className="btn bg-orange-400 btn-ghost-lg"
+                    >
+                      <FaEdit className="text-white"></FaEdit >
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-lg"
+                    >
+                      <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageItems;
