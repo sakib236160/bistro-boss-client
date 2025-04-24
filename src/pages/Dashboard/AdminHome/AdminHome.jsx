f35d954745f2831670fce1fa7f23bd3fc31f2433
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-24 12:17:01 +0600

    Load data and Display stats on the admin home

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,17 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaCar, FaDollarSign, FaJediOrder, FaUsers } from "react-icons/fa";
 
 const AdminHome = () => {
-    const {user} = useAuth();
-    return (
-        <div>
-            <h2 className="text-3xl">
-                <span>Hi, Welcome!</span>
-                {
-                    user?.displayName ? user.displayName: 'Back'
-                }
-            </h2>
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      <h2 className="text-3xl">
+        <span>Hi, Welcome!</span>
+        {user?.displayName ? user.displayName : "Back"}
+      </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-5xl text-orange-500"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
-    );
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+           <FaUsers className="text-5xl text-orange-500"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+           <FaBook className="text-5xl text-orange-500"></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats.menuItem}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaCar className="text-5xl text-orange-500"></FaCar>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
